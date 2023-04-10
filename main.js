b5537289df39bbac1d1d50fac1b2af9da2356ed6
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-10 17:31:11 +0200

    add logic to get color and render a grid

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+const newColor = document.getElementById("new-color");
+let colorArray = [];
 
+newColor.addEventListener("submit", getColor);
+
+function getColor(e) {
+  e.preventDefault();
+  const hexColor = document.querySelector("#color-picker").value.substring(1);
+  const colorAttribute = document.querySelector("#color-attribute").value;
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${hexColor}&mode=${colorAttribute}`
+  )
+    .then((res) => res.json())
+    .then((color) => {
+      colorArray = color.colors;
+      renderColor();
+    });
+}
+
+function renderColor() {
+  let html = "";
+  colorArray.forEach((color) => {
+    console.log(color.hex.value);
+    html += `
+        <span class="color" style="background-color:${color.hex.value}"></span>
+        <span class="bottom-text">${color.hex.value}</span>
+        `;
+    document.getElementById("grid-container").innerHTML = html;
+  });
+}
